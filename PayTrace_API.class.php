f28c53d63d7253f2d59eef36cea13a3b6252343b
Carl Victor Fontanos <carl.fontanos@gmail.com>
2017-10-02 22:39:59 -0400

    Added new methods.

--- PayTrace_API.class.php
+++ PayTrace_API.class.php
@@ -13,7 +13,7 @@ class PayTrace_API {
 	
 	protected $credentials;
 	
-	public function __construct() {
+	public function __construct( $client_username, $client_password ) {
 		
 		/** 
 		 * Normally you would include these information as variables when instantiating
@@ -21,8 +21,8 @@ class PayTrace_API {
 		 */
 		$this->credentials = (object) array(
 			'url'				=>	'https://paytrace.com/api/default.pay',
-			'client_username' 	=> 	'YOUR_USERNAME',
-			'client_password' 	=> 	'YOUR_PASSWORD'
+			'client_username' 	=> 	$client_username,
+			'client_password' 	=> 	$client_password
 		);
 	}
 	
@@ -62,7 +62,7 @@ class PayTrace_API {
 				'BADDRESS', 'BADDRESS2', 'BCITY', 'BSTATE', 'BZIP', 
 				'BCOUNTRY', 'SNAME', 'SADDRESS', 'SADDRESS2', 
 				'SCITY', 'SCOUNTY', 'SSTATE', 'SZIP', 'SCOUNTRY', 
-				'EMAIL', 'PHONE', 'FAX', 'CSC'
+				'EMAIL', 'PHONE', 'FAX', 'CUSTPSWD'
 			);
 			
 			foreach( $others as $key => $value ){
@@ -150,7 +150,7 @@ class PayTrace_API {
 	/**
 	 * @description: 	Authorize a customer profile in PayTrace
 	 * @documentation:	http://help.paytrace.com/api-authorizations
-	 * @example:		$paytrace_api->transaction_authorization( '0.00', 100000 );
+	 * @example:		$paytrace_api->transaction_authorization( $customer_id, $amount, array(...) );
 	 *
 	 */
 	public function transaction_authorization( $customer_id, $amount, $others = array() ){
@@ -187,10 +187,113 @@ class PayTrace_API {
 	}
 	
 	/**
+	 * @description: 	Process a Sale by customer ID (no re-enter credit card required)
+	 * @documentation:	http://help.paytrace.com/api-sale
+	 * @example:		$paytrace_api->transaction_sale( 50000, '2.28', array(...) );
+	 *
+	 */
+	public function transaction_sale( $customer_id, $amount, $others = array() ){
+		
+		if( $others ){
+			$valid_parameters = array(
+				'BNAME',' BADDRESS', 'BADDRESS2', 'BCITY', 'BSTATE', 
+				'BZIP', 'BCOUNTRY', 'SNAME', 'SADDRESS', 'SADDRESS2', 
+				'SCITY', 'SCOUNTY', 'SSTATE', 'SZIP', 'SCOUNTRY', 'EMAIL', 
+				'CSC', 'INVOICE', 'DESCRIPTION', 'TAX', 'CUSTREF', 'RETURNCLR', 
+				'CUSTOMDBA', 'ENABLEPARTIALAUTH', 'DISCRETIONARY DATA'
+			);
+			
+			foreach( $others as $key => $value ){
+				if( ! in_array( $key, $valid_parameters ) ){
+					return 'Invalid parameter "' . $key . '"'; 
+					break;
+				}
+			}
+		}
+		
+		$parameters = array_merge( $others, array(
+			'METHOD' 		=> 'ProcessTranx',
+			'TRANXTYPE' 	=> 'Sale',
+			'UN' 			=> $this->credentials->client_username,
+			'PSWD' 			=> $this->credentials->client_password,
+			'CUSTID' 		=> $customer_id,
+			'TERMS' 		=> 'Y',
+			'AMOUNT' 		=> $amount,
+		) );
+		
+		return $this->send( $parameters );
+		
+	}
+	
+	/**
+	 * @description: 	Get customer transactions from PayTrace
+	 * @documentation:	http://help.paytrace.com/api-export-transaction-information
+	 * @example:		
+			$paytrace_api->get_transactions( '05/01/2016', '05/31/2017', array(
+				'CUSTID'	=>	'100000', 
+				'RETURNBIN'	=>	'Y', 
+			) );
+	 *
+	 */
+	public function get_transactions( $start_date, $end_date, $others = array() ){
+		
+		if( $others ){
+			$valid_parameters = array(
+				'TRANXTYPE', 'CUSTID', 'USER', 'RETURNBIN', 'SEARCHTEXT'
+			);
+			
+			foreach( $others as $key => $value ){
+				if( ! in_array( $key, $valid_parameters ) ){
+					return 'Invalid parameter "' . $key . '"'; 
+					break;
+				}
+			}
+		}
+		
+		$parameters = array_merge( $others, array(
+			'METHOD' 		=> 'ExportTranx',
+			'UN' 			=> $this->credentials->client_username,
+			'PSWD' 			=> $this->credentials->client_password,
+			'TERMS' 		=> 'Y',
+			'SDATE' 		=> $start_date,
+			'EDATE' 		=> $end_date,
+			
+		) );
+		
+		return $this->send( $parameters, true );
+		
+	}
+	
+	/**
+	 * @description: 	Get single customer transaction from PayTrace
+	 * @documentation:	hhttp://help.paytrace.com/api-export-transaction-information
+	 * @example:		
+			$paytrace_api->get_transaction( $transaction_id );
+	 *
+	 */
+	public function get_transaction( $transaction_id ){
+		
+		$parameters = array(
+			'METHOD' 		=> 'ExportTranx',
+			'UN' 			=> $this->credentials->client_username,
+			'PSWD' 			=> $this->credentials->client_password,
+			'TERMS' 		=> 'Y',
+			'TRANXID' 		=> $transaction_id,
+		);
+		
+		if( isset( $this->send( $parameters, true )->TRANSACTIONRECORD[0] ) ){
+			return $this->send( $parameters, true )->TRANSACTIONRECORD[0];
+		}
+		
+		return $this->send( $parameters, true );
+		
+	}
+	
+	/**
 	 * @description: Handles sending of information to PayTrace API using cURL method
 	 *
 	 */
-	public function send( $parmlist_array ) {
+	public function send( $parmlist_array, $multiple = false ) {
 		
 		$parmlist_formatted = $this->format_request( $parmlist_array );
 		$ch = curl_init();
@@ -208,39 +311,90 @@ class PayTrace_API {
 		
 		curl_close( $ch );
 		
-		return $this->format_response( $response );
+		// return $response;
+		
+		if( $multiple ){
+			return $this->format_response( $response );
+		}
+		
+		return $this->format_single_response( $response );
+		
 	}
 	
 	/**
-	 * @description: Converts the response into a readable array
+	 * @description: Converts the response into a readable object
+	 *
+	 */
+	public function format_single_response( $response ){
+		
+		$response_array = array();
+		/* Loop through the items then store them in an array */
+		foreach( explode( '|', $response ) as $response_string ){
+			
+			if( $response_string ){
+				$pieces = explode('~', $response_string );
+				
+				/* Check if value contains + or =, which basically means that the value is an array */
+				if ( strpos( $pieces[1], '+' ) !== false && strpos( $pieces[1], '=' ) !== false ){
+					$sub_response_array = array();
+					
+					/* Loop through the sub items then store them in an array */
+					foreach( explode( '+', $pieces[1] ) as $sub_response_string ){
+						$sub_pieces = explode( '=', $sub_response_string );
+						
+						if( isset( $sub_pieces[0] ) && isset( $sub_pieces[1] ) ){
+							$sub_response_array[$sub_pieces[0]] = $sub_pieces[1];
+						}
+					}
+					
+					$pieces[1] = $sub_response_array;
+				}
+				
+				$response_array[$pieces[0]] = $pieces[1];
+			}
+		}
+		
+		/* Convert into an object then return */
+		return json_decode( json_encode( $response_array ) );
+		
+	}
+	
+	/**
+	 * @description: Converts multiple response into one readable object
 	 *
 	 */
 	public function format_response( $response ){
 		
-		/* If we got a response containing the string "RESPONSE~", then it means that our request succeeded */
-		if( strpos( $response, 'RESPONSE~' ) !== false ){
-			/* We return "1" to indicate success  */
-			return 1;
-		}
-		
-		/* If we got a response containing the string "ERROR~", then it means that our request failed */
-		if ( strpos( $response, 'ERROR~' ) !== false ){
-			/* We clean the error message then return it */
-			$response = explode( '. ', str_replace( '|', '', $response ) )[1];
-			return $response;
+		$response_array = array();
+		/* Loop through the items then store them in an array */
+		foreach( explode( '|', $response ) as $response_string ){
 			
-		}
-		
-		$formatted = array();
-		
-		foreach ( explode( '+', $response ) as $code ){
-			$pieces = explode( '=', $code );
-			if( isset( $pieces[0] ) && isset( $pieces[1] ) ){
-				$formatted[$pieces[0]] = $pieces[1];
+			if( $response_string ){
+				$pieces = explode('~', $response_string );
+				
+				/* Check if value contains + or =, which basically means that the value is an array */
+				if ( strpos( $pieces[1], '+' ) !== false && strpos( $pieces[1], '=' ) !== false ){
+					$sub_response_array = array();
+					
+					/* Loop through the sub items then store them in an array */
+					foreach( explode( '+', $pieces[1] ) as $sub_response_string ){
+						$sub_pieces = explode( '=', $sub_response_string );
+						
+						if( isset( $sub_pieces[0] ) && isset( $sub_pieces[1] ) ){
+							$sub_response_array[$sub_pieces[0]] = $sub_pieces[1];
+						}
+					}
+					
+					$pieces[1] = $sub_response_array;
+					
+					/* Store formed arrays into one array */
+					$response_array[$pieces[0]][] = $pieces[1];
+				}
 			}
 		}
 		
-		return $formatted;
+		/* Convert into an object then return */
+		return json_decode( json_encode( $response_array ) );
 		
 	}
 	
@@ -265,7 +419,31 @@ class PayTrace_API {
 } 
 
 /* Make global */
-$paytrace_api = new PayTrace_API;
+// $paytrace_api = new PayTrace_API('YOUR_USERNAME', 'YOU_PASSWORD');
 
 // echo '<pre>';
-// print_r( $paytrace_api->get_user( 150003 ) );
+
+// print_r( $paytrace_api->get_user( 200005 ) );
+
+// print_r( $paytrace_api->transaction_sale( 2, '4.49', array(
+	// 'CSC'		=>	'998',
+	// 'INVOICE'	=>	'10112',
+// ) ) );
+
+// print_r( $paytrace_api->transaction_authorization( 200015, '0.00' ) );
+
+// print_r( $paytrace_api->get_transactions( '05/01/2015', '05/31/2017', array(
+	// 'CUSTID'	=>	'49',
+	// 'RETURNBIN'	=>	'Y',
+// ) ) );
+
+// exit();
+
+// $transactions = $paytrace_api->get_transactions( '05/01/2015', '05/31/2017', array( 'CUSTID' => '49', 'RETURNBIN' => 'Y', ) )->TRANSACTIONRECORD;
+
+// echo '<pre>';
+// print_r( get_paytrace_invoice_details( $transactions, 40497 ) );
+
+// print_r( $paytrace_api->get_transaction( 172171535 ) );
+
+// exit();
